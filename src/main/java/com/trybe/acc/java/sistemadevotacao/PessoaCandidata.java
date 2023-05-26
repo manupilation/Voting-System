99efe6e8d2a78375e1f8a672beef804fe44909ce
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-05-26 14:58:23 -0300

    feat: comentario Javadoc PessoaCandidata

--- src/main/java/com/trybe/acc/java/sistemadevotacao/PessoaCandidata.java
+++ src/main/java/com/trybe/acc/java/sistemadevotacao/PessoaCandidata.java
@@ -1,5 +1,41 @@
 package com.trybe.acc.java.sistemadevotacao;
 
-public class PessoaCandidata {
+/** 
+ * Classe que representa uma pessoa candidata a eleição.
+ **/
+public class PessoaCandidata extends Pessoa {
+  
+  int numero;
+  
+  int votos = 0;
+
+  /** 
+   * Construtor Pessoa Candidata. Precisa de nome e número do candidato.
+   **/
+  public PessoaCandidata(String nome, int numero) {
+    super(nome);
+    
+    this.numero = numero;
+  }
+
+  public int getNumero() {
+    return numero;
+  }
+
+  public void setNumero(int numero) {
+    this.numero = numero;
+  }
+
+  public int getVotos() {
+    return this.votos;
+  }
+
+  public void setVotos() {
+    this.votos += 1;
+  }
+  
+  public void receberVoto() {
+    setVotos();
+  }
 
 }
