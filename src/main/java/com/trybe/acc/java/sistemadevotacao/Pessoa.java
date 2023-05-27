7c438263d2e92289044ddadf5101b510bdd6c3e7
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-05-27 13:46:35 -0300

    feat: abstract class Pessoa

--- src/main/java/com/trybe/acc/java/sistemadevotacao/Pessoa.java
+++ src/main/java/com/trybe/acc/java/sistemadevotacao/Pessoa.java
@@ -1,5 +1,23 @@
 package com.trybe.acc.java.sistemadevotacao;
 
-public class Pessoa {
+/** 
+ * Classe que representa uma pessoa.
+ **/
+public abstract class Pessoa {
+
+  public Pessoa(String nome) {
+    super();
+    this.nome = nome;
+  }
+
+  protected String nome;
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
 
 }
